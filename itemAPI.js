d8bdece43e3e2ae8b6f42a9d10f1d44578f2271a
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2021-03-03 02:29:56 +1000

    test pog

--- itemAPI.js
+++ itemAPI.js
@@ -5,7 +5,7 @@ let confirmChoice;
 // * Functions
 function searchItem() {
     // Gets wanted Item ID
-    wantedItemID = prompt("What is the ID of the item your searching for? (e.g. 001,002,003)")
+    wantedItemID = prompt("What is the ID of the item your searching for? (e.g. 1,2,3)")
     confirmChoice = confirm("Is #" + wantedItemID + " the item ID you wanted?")
 
     // Search For Item
@@ -15,19 +15,44 @@ function searchItem() {
             ----------------------------------
             EXAMPLE ITEM:
             newItem(
+                "USERS_GITHUB_NAME"
+                "ID",
                 "NAME",
-                "itemImg/ITEM_ICON",
+                "ITEM_ICON",
+                "CAPTION",
                 "DESCRIPTION"
             );
         */
         newItem(
+            "MASTRIO",
+            "1",
             "test dummy",
-            "itemImg/apple.png",
+            "apple.png",
+            "bean man go brrrrrrrrrrrrr",
             "A big fat apple that can go poo on the loo doo doo dee doo hahahaha"
+        );
+        newItem(
+            "MASTRIO",
+            "2",
+            "The second test I made pog",
+            "apple.png",
+            "holy moly it's a poop",
+            "So we back in the mine<br>Got our pickaxe swinging from side to side<br>Side-side to side<br>This task, a grueling one<br>Hope to find some diamonds tonight, night, night<br>Diamonds tonight"
         );
     }
 }
 
-function newItem(name,iconPath,description) {
-    
+function newItem(userName,ID,name,iconPath,caption,description) {
+    if (ID == wantedItemID) {
+        // Display Name
+        document.getElementById("display-Name").innerHTML = "[#" + ID + "] " + name;
+        // Contributer
+        document.getElementById("display-Contributer").innerHTML = "Contributed by: " + userName;
+        // Caption
+        document.getElementById("display-Caption").innerHTML = '<i>"' + caption + '"</i>';
+        // Icon
+        document.getElementById("display-Icon").innerHTML = '<img src="itemImg/' + iconPath + '"></img>';
+        // Icon
+        document.getElementById("display-Description").innerHTML = description;
+    }
 }
