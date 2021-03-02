06474a651e935efc4534084050076f719f6ffa85
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2021-03-03 02:31:59 +1000

    nukes are always a good idea -Ranboo 2021

--- itemAPI.js
+++ itemAPI.js
@@ -10,35 +10,7 @@ function searchItem() {
 
     // Search For Item
     if (confirmChoice == true) {
-        /* 
-            PUT ALL ITEMS FOR THE API IN HERE:
-            ----------------------------------
-            EXAMPLE ITEM:
-            newItem(
-                "USERS_GITHUB_NAME"
-                "ID",
-                "NAME",
-                "ITEM_ICON",
-                "CAPTION",
-                "DESCRIPTION"
-            );
-        */
-        newItem(
-            "MASTRIO",
-            "1",
-            "test dummy",
-            "apple.png",
-            "bean man go brrrrrrrrrrrrr",
-            "A big fat apple that can go poo on the loo doo doo dee doo hahahaha"
-        );
-        newItem(
-            "MASTRIO",
-            "2",
-            "The second test I made pog",
-            "apple.png",
-            "holy moly it's a poop",
-            "So we back in the mine<br>Got our pickaxe swinging from side to side<br>Side-side to side<br>This task, a grueling one<br>Hope to find some diamonds tonight, night, night<br>Diamonds tonight"
-        );
+       itemDatabase();
     }
 }
 
@@ -55,4 +27,38 @@ function newItem(userName,ID,name,iconPath,caption,description) {
         // Icon
         document.getElementById("display-Description").innerHTML = description;
     }
+}
+
+function itemDatabase() {
+    /* 
+            PUT ALL ITEMS FOR THE API IN HERE:
+            ----------------------------------
+            EXAMPLE ITEM:
+            newItem(
+                "USERS_GITHUB_NAME"
+                "ID",
+                "NAME",
+                "ITEM_ICON",
+                "CAPTION",
+                "DESCRIPTION"
+            );
+    */
+    // Test 1
+    newItem(
+        "MASTRIO",
+        "1",
+        "test dummy",
+        "apple.png",
+        "bean man go brrrrrrrrrrrrr",
+        "A big fat apple that can go poo on the loo doo doo dee doo hahahaha"
+    );
+    // Test 2
+    newItem(
+        "MASTRIO",
+        "2",
+        "The second test I made pog",
+        "apple.png",
+        "holy moly it's a poop",
+        "So we back in the mine<br>Got our pickaxe swinging from side to side<br>Side-side to side<br>This task, a grueling one<br>Hope to find some diamonds tonight, night, night<br>Diamonds tonight"
+    );
 }
